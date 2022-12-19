011a653b9a90c0d35833526ca85bafefad2ba404
Maciej Malycha <koadjunky@gmail.com>
2022-12-19 22:22:55 +0100

    Initial commit (project structure)

---
+++ README.md
@@ -0,0 +1,17 @@
+# hazelcast-poc
+
+## Docker Compose
+
+Start docker compose:
+
+```docker compose up```
+
+Shut docker compose:
+
+```docker compose down```
+
+## InfluxDB
+
+InfluxDB console is available at:
+
+```http://localhost:8086/``` (admin/adminuser)
---
+++ influxdb-poc-test/src/test/java/eu/malycha/influxdb/poc/test/CrudTest.java
@@ -0,0 +1,26 @@
+package eu.malycha.influxdb.poc.test;
+
+import org.junit.jupiter.api.Test;
+
+class CrudTest {
+
+    @Test
+    void addRecord() {
+
+    }
+
+    @Test
+    void deleteRecord() {
+
+    }
+
+    @Test
+    void queryRecord() {
+
+    }
+
+    @Test
+    void queryRecordKeys() {
+
+    }
+}
