f3420fcb3280d0fe6b2dc38954421d42a3a1120a
Maciej Malycha <koadjunky@gmail.com>
2022-12-23 20:51:45 +0100

    Delete records

--- influxdb-poc-test/src/test/java/eu/malycha/influxdb/poc/test/CrudTest.java
+++ influxdb-poc-test/src/test/java/eu/malycha/influxdb/poc/test/CrudTest.java
@@ -1,21 +1,39 @@
 package eu.malycha.influxdb.poc.test;
 
+import com.influxdb.client.DeleteApi;
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
 import com.influxdb.client.WriteApiBlocking;
 import com.influxdb.client.domain.WritePrecision;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
 
 
 class CrudTest {
 
-    @Test
-    void addRecord() {
+    static InfluxDBClient influxDBClient;
+    static String org = "dev";
+    static String bucket = "db0";
+
+    @BeforeAll
+    static void setUp() {
         char [] token = "admin-secret-token".toCharArray();
-        InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token, "dev", "db0");
+        influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token, org, bucket);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        influxDBClient.close();
+    }
+
+    @Test
+    void addRecord_synchronousBlocking() {
         WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
 
         Exposure exposure = Exposure.newBuilder()
@@ -30,7 +48,12 @@ class CrudTest {
 
     @Test
     void deleteRecord() {
+        DeleteApi deleteApi = influxDBClient.getDeleteApi();
 
+        OffsetDateTime start = OffsetDateTime.now().minus(1, ChronoUnit.YEARS);
+        OffsetDateTime stop = OffsetDateTime.now();
+
+        deleteApi.delete(start, stop, "currency = \"USD\"", bucket, org);
     }
 
     @Test
